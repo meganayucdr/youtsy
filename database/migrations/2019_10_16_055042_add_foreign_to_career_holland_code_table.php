e893ee21c6df69de158d6cc5c98138071eb40f98
meganayucdr <mgncdr@gmail.com>
2019-10-18 15:44:41 +0700

    career and holland code page done

---
+++ database/migrations/2019_10_16_055042_add_foreign_to_career_holland_code_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignToCareerHollandCodeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('career_holland_code', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('career_holland_code', function (Blueprint $table) {
+            $table->foreign('career_id')->references('id')->on('careers');
+            $table->foreign('holland_code_id')->references('id')->on('holland_codes');
+        });
+    }
+}
