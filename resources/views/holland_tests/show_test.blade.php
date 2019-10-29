0836dcce4450f7e4f98a165996384a71f3542873
meganayucdr <mgncdr@gmail.com>
2019-10-29 16:21:00 +0700

    show test page styling

--- resources/views/holland_tests/show_test.blade.php
+++ resources/views/holland_tests/show_test.blade.php
@@ -1,12 +1,24 @@
 @extends('layouts.app')
 
+@push('head')
+    <style>
+        .btn.active span.fa {
+            visibility: visible;
+        }
+
+        .btn span.fa {
+            visibility: hidden;
+        }
+    </style>
+@endpush
+
 @section('content')
     <div class="container">
         <div class="align-center">
             <h1>Mulai Tes Sekarang!</h1>
         </div>
         {{ Form::open(array('route' => 'holland_tests.store_user_test')) }}
-            {{ Form::token() }}
+
             @foreach( $questions as $question )
                 <h3>{{ $question->question }}</h3>
                 <input type="hidden" name="questions_id[]" value="{{ $question->id }}">
@@ -15,7 +27,7 @@
                         @foreach( $options as $option )
                             <label class="btn btn-lg btn-secondary" onclick="showCheck({{ $option->id }})">
                                 {{ Form::radio('option_id', $option->id, true, ['autocomplete' => 'off']) }}
-                                <i class="fa fa-check" aria-hidden="true" id="check-{{$option->id}}" style="visibility: hidden;"></i>
+                                <span class="fa fa-check" aria-hidden="true" style="visibility: hidden"></span>
                             </label>
                         @endforeach
                     </div>
@@ -26,13 +38,14 @@
             @endif
             {{ $questions->links() }}
         {{ Form::close() }}
-    </div>
-@endsection
 
-@push('body')
-    <script>
+    </div>
+    <script type="text/javascript">
         function showCheck(id) {
-            document.getElementById('check-' + id).style.visibility = 'visible';
+            if (document.getElementById('label-' + id).button('toggle') === true)
+                document.getElementById('check-' + id).style.visibility = 'visible';
         }
     </script>
-@endpush
+@endsection
+
+
