b5b5b2c303ca7aa7d48714ec77afb58ddecca36f
meganayucdr <mgncdr@gmail.com>
2019-11-01 16:08:44 +0700

    working on pagination form

--- resources/views/holland_tests/show_test.blade.php
+++ resources/views/holland_tests/show_test.blade.php
@@ -5,12 +5,15 @@
         <div class="align-center py-5">
             <h1 class="text-center">Mulai Tes Sekarang!</h1>
         </div>
+        {{ Session::push('test.question', []) }}
+        {{ Session::push('test.answer', []) }}
         {{ Form::open(array('route' => 'holland_tests.store_user_test')) }}
             {{ csrf_field() }}
             @foreach( $questions as $question )
                 <div class="py-4">
                     <h3 class="text-center">{{ $question->question }}</h3>
                     <input type="hidden" name="questions_id[]" value="{{ $question->id }}" class="form-control">
+                    {{ Session::push('test.question', $question->id) }}
                     <div class="row justify-content-center">
                         <div class="col-2">
                             <p class="text-left text-md-right">Sangat Tidak Setuju</p>
@@ -21,6 +24,7 @@
                                 {{ Form::radio('options_id['. $question->id . ']', $option->id, false) }}
                                 <span class="checkmark"></span>
                             </label>
+                                {{ Session::push('test.question', $options_id[$question->id]) }}
                         @endforeach
                         </div>
                         <div class="col-2">
@@ -28,6 +32,7 @@
                         </div>
                     </div>
                 </div>
+                {{ Session::push() }}
             @endforeach
             @if( $questions->currentPage() == $questions->lastPage() )
                 <div class="text-center pb-5">
